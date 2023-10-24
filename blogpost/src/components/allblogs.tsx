b05bac682c30bc53062adce7075cb8f88f656fca
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-10-24 21:21:26 +0500

    all blogs

--- blogpost/src/components/allblogs.tsx
+++ blogpost/src/components/allblogs.tsx
@@ -9,7 +9,7 @@ export default function AllBlog() {
           <h2 className="card-title">Card title!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <Link href="/edit/1" className="hover:underline">
+            <Link href="/blog/1" className="hover:underline">
               Read more ...
             </Link>
           </div>
