48c89aa8ae789f57ac3c5ef24113bc1283ecfc1f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-10-24 21:21:17 +0500

    reconstruct the folder

--- blogpost/src/app/(route)/edit/[id]/page.tsx
+++ blogpost/src/app/(route)/edit/[id]/page.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import PostBlog from "../../../../generic/postblog";
 import { SubmitHandler } from "react-hook-form";
 import { TypeInterface } from "../../../../../types/type";
+import ButtonAction from "../../../../components/buttonaction";
 export default function EditPage() {
   const onSubmit: SubmitHandler<TypeInterface> = (data) => console.log(data);
 
   return (
     <div>
+      {/* <div className="container">
+        <h2>post One</h2>
+        <ButtonAction />
+        <p className="text-slate-700">Post one content</p>
+      </div> */}
       <PostBlog Submitform={onSubmit} Edit={true} />
     </div>
   );
