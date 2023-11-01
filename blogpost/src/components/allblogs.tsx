fdebe45ea54567a49da358be25a68bf134e7d165
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-11-01 20:52:17 +0500

    integrate server component

--- blogpost/src/components/allblogs.tsx
+++ blogpost/src/components/allblogs.tsx
@@ -9,14 +9,13 @@ async function getPosts() {
 
 export default async function AllBlog() {
   const posts = await getPosts();
-  console.log(posts);
 
-  return (
-    <div className="grid grid-cols-1  md:grid-cols-3 gap-4 mt-10">
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
+  const renderPost = posts?.map(({ id, title }) => {
+    return (
+      <div className="card w-80 md:w-96 bg-base-100 shadow-xl" key={id}>
         <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title text-red-700">{title}</h2>
+          <p className=""></p>
           <div className="card-actions justify-end">
             <Link href="/blog/1" className="hover:underline">
               Read more ...
@@ -24,53 +23,12 @@ export default async function AllBlog() {
           </div>
         </div>
       </div>
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+    );
+  });
 
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+  return (
+    <div className="flex justify-center text-center items-center gap-3 my-10">
+      {renderPost}
     </div>
   );
 }
